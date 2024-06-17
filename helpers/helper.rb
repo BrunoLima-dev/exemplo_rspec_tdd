f0a914a8cb6069793b4d5a81aa384da238abb67f
BrunoLima-dev <brunoprograme04@gmail.com>
2024-06-17 20:43:21 -0300

    add methods helpers arbitrarios

---
+++ helpers/helper.rb
@@ -0,0 +1,7 @@
+# Este é um modulo especifico para testes de composição, por isso que esta sendo
+# criado aqui junto na mesma paste do teste de composição.
+module Helper
+  def fruta
+    %w(banana laranja uva).sample
+  end
+end
--- spec/composicao/composicao_spec.rb
+++ spec/composicao/composicao_spec.rb
@@ -2,7 +2,10 @@ describe 'Ruby on Rails' do
   it { is_expected.to start_with("Ruby").and end_with("Rails") }
   it { expect(fruta).to eq('banana').or eq('laranja').or eq('uva') }
 
-  def fruta
-    %w(banana laranja uva).sample
-  end
+  # helper method arbitrário criei um modulo e adicionei este metodo lá
+  # que esta dentro da pasta helpers.
+
+  # def fruta
+  #   %w(banana laranja uva).sample
+  # end
 end
