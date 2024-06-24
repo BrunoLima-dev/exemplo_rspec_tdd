a5c9c5508b6837db8ebeb1cde0209ef63e05cdd2
BrunoLima-dev <brunoprograme04@gmail.com>
2024-06-24 11:16:43 -0300

    feat: Add negated matchers for include and be_within
    The code changes include defining negated matchers for the `include` and `be_within` matchers in the `arrays_spec.rb` and `be_within_spec.rb` files respectively. These negated matchers allow for checking if an element is not included in an array and if a number is not within a certain range. This addition enhances the flexibility and expressiveness of the test assertions.

--- spec/matchers/arrays/arrays_spec.rb
+++ spec/matchers/arrays/arrays_spec.rb
@@ -1,7 +1,11 @@
+RSpec::Matchers.define_negated_matcher :exclude, :include # define um matcher negado
+
 describe Array.new([1, 2, 3]), 'Array testing' do
   it '#include' do
     expect(subject).to include(2, 3) # verifica se contain os elementos separados
   end
+
+  it { expect(subject).to exclude(4)} # verifica se não contém o elemento
 
   it '#contain_exactly' do
     expect(subject).to contain_exactly(1, 2, 3) # verifica se contain os elementos exatos, mas não importa a ordem
