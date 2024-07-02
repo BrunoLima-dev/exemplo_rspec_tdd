5401b2bfc868d524863bc0269840efd06edb72f6
BrunoLima-dev <brunoprograme04@gmail.com>
2024-07-02 10:56:21 -0300

    feat: Here we use dynamic arguments and test on any class instance and also force an error so we can if we want to do a treatment.

--- spec/stubs/stubs_spec.rb
+++ spec/stubs/stubs_spec.rb
@@ -13,4 +13,39 @@ describe 'Stub' do
     course_finished = student.has_finished?(course)
     expect(course_finished).to be_truthy
   end
+
+  it 'Argumentos dinâmicos' do
+    student = Student.new
+
+    allow(student).to receive(:foo) do |arg|
+      if arg == :hello
+        'Olá'
+      elsif arg == :hi
+        'Hi!!!'
+      end
+    end
+
+    expect(student.foo(:hello)).to eq('Olá')
+    expect(student.foo(:hi)).to eq('Hi!!!')
+  end
+
+  it 'Qualquer instância de classe' do
+    student = Student.new
+    other_student = Student.new
+
+    allow_any_instance_of(Student).to receive(:bar).and_return(true)
+
+    expect(student.bar).to be_truthy
+    expect(other_student.bar).to be_truthy
+  end
+
+  # Aqui estamos simulando um erro, se quisermos podemos fazer um tratamento de erro
+  it 'Erros' do
+    student = Student.new
+
+    allow(student).to receive(:bar).and_raise(RuntimeError)
+
+    expect{ student.bar }.to raise_error(RuntimeError)
+    puts 'Erro capturado'
+  end
 end
