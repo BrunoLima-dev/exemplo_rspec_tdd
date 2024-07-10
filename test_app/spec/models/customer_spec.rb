b08a7e4d8d65a39daf03e7f85c9b9700b02eb306
BrunoLima-dev <brunoprograme04@gmail.com>
2024-07-10 14:30:22 -0300

    feat: using fixtures I create a .yml file and use it in model tests.

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :customers
+
+  it 'Createa Customer' do
+    customer = customers(:bruno)
+
+    # subject.name = 'Bruno Lima'
+    # subject.email = 'brunolima@gmail.com' // podemos troca essa parte por FIXTURES
+    # subject.save
+
+    expect(customer.full_name).to eq('Sr. Bruno Lima')
+  end
 end
