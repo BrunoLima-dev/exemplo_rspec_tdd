feacc278b9f11f15eb2acb3455d3832d900e6efd
BrunoLima-dev <brunoprograme04@gmail.com>
2024-07-15 19:04:32 -0300

    feat: Add factory_bot_rails gem for test_app
    This commit adds the `factory_bot_rails` gem to the `Gemfile` of the `test_app` project. This gem is necessary for using FactoryBot to create test data in the project's model tests.

---
+++ test_app/spec/factories/customer.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :customer do
+    name { "Bruno Lima" }
+    email { "brunolima@gmail.com" }
+  end
+end
--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,15 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  fixtures :customers
 
   it 'Createa Customer' do
-    customer = customers(:bruno)
-
-    # subject.name = 'Bruno Lima'
-    # subject.email = 'brunolima@gmail.com' // podemos troca essa parte por FIXTURES
-    # subject.save
-
+    customer = create(:customer)
     expect(customer.full_name).to eq('Sr. Bruno Lima')
   end
 end
