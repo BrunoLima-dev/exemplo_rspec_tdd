941ba4da5bc5ca2a889f4bfe47be23cf65fd8779
BrunoLima-dev <brunoprograme04@gmail.com>
2024-07-15 21:00:03 -0300

    feat: Update customer factory to include aliases
    This commit updates the customer factory in the `customer.rb` file to include aliases for the `:user` factory. This change allows us to use both `:customer` and `:user` interchangeably when creating test data.

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
 
   it 'Createa Customer' do
-    customer = create(:customer)
+    customer = create(:user) # ou create(:customer)
     expect(customer.full_name).to start_with('Sr. ')
+  end
+
+  it 'full_name - Sobrescrevendo atributo' do
+    customer = create(:customer, name: 'Bruno Lima')
+    expect(customer.full_name).to start_with('Sr. Bruno Lima')
   end
 
   it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
