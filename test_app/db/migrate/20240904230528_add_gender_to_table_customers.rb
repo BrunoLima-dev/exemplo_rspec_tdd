2fcd36cb1ba9eec9b2ec3b18e287b8ed3f2682fb
BrunoLima-dev <brunoprograme04@gmail.com>
2024-09-05 20:35:09 -0300

    Add gender column to customers table e faço teste com FactoryBot

---
+++ test_app/db/migrate/20240904230528_add_gender_to_table_customers.rb
@@ -0,0 +1,5 @@
+class AddGenderToTableCustomers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :customers, :gender, :string
+  end
+end
--- test_app/spec/factories/customers.rb
+++ test_app/spec/factories/customers.rb
@@ -12,6 +12,24 @@ FactoryBot.define do
 
     sequence(:email) { |n| "meu_email-#{n}@email.com" }
 
+    trait :male do
+      gender { 'M' }
+    end
+
+    trait :female do
+      gender { 'F' }
+    end
+
+    trait :vip do
+      vip { true }
+      days_to_pay { 30 }
+    end
+
+    trait :default do
+      vip { false }
+      days_to_pay { 15 }
+    end
+
     factory :customer_vip do
       vip { true }
       days_to_pay { 30 }
@@ -30,7 +48,7 @@ FactoryBot.define do
 
     factory :customer_with_orders, traits: [:with_orders]
     factory :customer_male, traits: [:male]
-    factory :customer_female, traits: [:famele]
+    factory :customer_female, traits: [:female]
 
     after(:create) do |customer, evaluator|
       customer.name.upcase! if evaluator.upcased # Se upcased for true, name recebe o valor em maiúsculo
