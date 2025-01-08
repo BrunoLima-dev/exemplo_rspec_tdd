0be49342006037784e22076dc04de79e38ecabd2
BrunoLima-dev <brunoprograme04@gmail.com>
2025-01-08 20:00:31 -0300

    feat: adicionar gem 'launchy' para testes de interface do usuário e atualizar comentários em testes de clientes

--- test_app/spec/features/customers_spec.rb
+++ test_app/spec/features/customers_spec.rb
@@ -4,7 +4,8 @@ RSpec.feature "Customers", type: :feature, js: true do
   it 'Visit index page' do
     visit(customers_path)
     # print(page.html) # mostrar o html da página no terminal
-    # save_and_open_page # salva a página em um arquivo
+    # save_and_open_page # salva a página em um arquivo e abrir
+    # page.save_and_open_screenshot('screenshot1.png') # salva e abre o screenshot
     page.save_screenshot('screenshot.png')
     expect(page).to have_current_path(customers_path)
   end
