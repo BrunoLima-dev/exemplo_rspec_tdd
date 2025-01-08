003b71a0943fcaaad0332c8fb4cadbf344c2d375
BrunoLima-dev <brunoprograme04@gmail.com>
2025-01-08 19:12:27 -0300

    feat: adiciono configuração para usar chromedrive

--- test_app/spec/features/customers_spec.rb
+++ test_app/spec/features/customers_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "Customers", type: :feature do
+RSpec.feature "Customers", type: :feature, js: true do
   it 'Visit index page' do
     visit(customers_path)
-    print(page.html) # mostrar o html da página no terminal
+    # print(page.html) # mostrar o html da página no terminal
     # save_and_open_page # salva a página em um arquivo
-    # page.save_screenshot('customers.png')
+    page.save_screenshot('screenshot.png')
     expect(page).to have_current_path(customers_path)
   end
 end
